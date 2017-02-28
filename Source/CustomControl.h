95f94e6013cb32e9277ec0d75d0a8f8e25479e27
Eric Baker <ericb0774@gmail.com>
2017-02-28 06:10:09 -0600

    Creates Xcode project

---
+++ Source/CustomControl.h
@@ -0,0 +1,19 @@
+//
+//  CustomControl.h
+//  CustomControl
+//
+//  Created by Eric Baker on 28/Feb/2017.
+//  Copyright © 2017 Samples. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number for CustomControl.
+FOUNDATION_EXPORT double CustomControlVersionNumber;
+
+//! Project version string for CustomControl.
+FOUNDATION_EXPORT const unsigned char CustomControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomControl/PublicHeader.h>
+
+
