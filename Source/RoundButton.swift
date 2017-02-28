58115adeb33f369b38bb3bedba537dce21487ed5
Eric Baker <ericb0774@gmail.com>
2017-02-28 06:47:20 -0600

    Makes class open

--- Source/RoundButton.swift
+++ Source/RoundButton.swift
@@ -8,7 +8,7 @@ import Foundation
 import UIKit
 
 @IBDesignable
-public class RoundButton: UIView {
+open class RoundButton: UIView {
 
     @IBInspectable public var color: UIColor
 
@@ -22,17 +22,17 @@ public class RoundButton: UIView {
         super.init(coder: aDecoder)
     }
 
-    public override class var requiresConstraintBasedLayout: Bool {
+    open override class var requiresConstraintBasedLayout: Bool {
         get {
             return true
         }
     }
 
-    public override func draw(_ rect: CGRect) {
+    open override func draw(_ rect: CGRect) {
         let size = min(rect.size.width, rect.size.height)
         let center = CGPoint(x: rect.size.width / 2.0, y: rect.size.height / 2.0)
         let frame = CGRect(origin: center, size: CGSize(width: size, height: size))
-        
+
         let path = UIBezierPath(ovalIn: frame)
         self.color.setFill()
         path.fill()
